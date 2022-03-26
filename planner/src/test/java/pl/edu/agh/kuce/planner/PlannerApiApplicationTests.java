6292192c28616a247f041ca021abd5e5253f8364
Marcin Szubert <marszub@gmail.com>
2022-03-26 18:38:31 +0100

    Project init v2 rest files

---
+++ planner/src/test/java/pl/edu/agh/kuce/planner/PlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.kuce.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
