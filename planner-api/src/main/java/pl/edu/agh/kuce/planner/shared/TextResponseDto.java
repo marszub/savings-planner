7b3a9ee5e42a36eb441884a39d0172e2866c7c2c
MaksymilianK <kondziugeniusz@gmail.com>
2022-04-01 22:55:19 +0200

    Change TextResponseDto to record

--- planner-api/src/main/java/pl/edu/agh/kuce/planner/shared/TextResponseDto.java
+++ planner-api/src/main/java/pl/edu/agh/kuce/planner/shared/TextResponseDto.java
@@ -1,20 +1,3 @@
 package pl.edu.agh.kuce.planner.shared;
 
-public class TextResponseDto {
-
-    private String message;
-
-    public TextResponseDto() {}
-
-    public TextResponseDto(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+public record TextResponseDto(String message) {}
