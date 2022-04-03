84140c23102c99ebf29ae903dfe00216b91b4643
Ela Cymerys <ela.cymerys@gmail.com>
2022-04-03 10:30:18 +0200

    Add authorization routes

--- planner-front/src/App.js
+++ planner-front/src/App.js
@@ -1,6 +1,15 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import SignIn from "./Components/Auth/SignIn";
+import SignUp from "./Components/Auth/SignUp";
+
 function App() {
   return (
-    null
+      <Router>
+        <Switch>
+          <Route path='/sign-in' component={ SignIn } />
+          <Route path='/sign-up' component={ SignUp } />
+        </Switch>
+      </Router>
   );
 }
 
