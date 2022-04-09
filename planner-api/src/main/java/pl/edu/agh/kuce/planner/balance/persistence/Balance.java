e83ff2331089d44ff7cc55e8ef488fa4f2ecfb57
Mikolaj <m_zasada@me.com>
2022-04-09 13:06:57 +0200

    Creating database structre of account balance and also cover them with tests

--- planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/persistence/Balance.java
+++ planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/persistence/Balance.java
@@ -1,4 +1,54 @@
 package pl.edu.agh.kuce.planner.balance.persistence;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name="balances")
 public class Balance {
+    @Id
+    @Column(nullable = false)
+    private Integer userId;
+
+    @Column(nullable = false)
+    private Integer balance;
+
+    public Balance(){}
+
+    public Balance(Integer userId, Integer balance){
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    void setUserId(Integer userId){
+        this.userId = userId;
+    }
+
+    void setBalance(Integer balance){
+        this.balance = balance;
+    }
+
+    Integer getUserId(){
+        return this.userId;
+    }
+
+    Integer getBalance(){
+        return this.balance;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Balance balance = (Balance) o;
+        return Objects.equals(userId, balance.userId) && Objects.equals(this.balance, balance.balance);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(userId);
+    }
 }
--- planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/persistence/BalanceRepository.java
+++ planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/persistence/BalanceRepository.java
@@ -1,4 +1,12 @@
 package pl.edu.agh.kuce.planner.balance.persistence;
 
-public interface BalanceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+
+public interface BalanceRepository extends JpaRepository<Balance, Integer> {
+
+    @Query("SELECT b FROM Balance b WHERE b.userId = ?1")
+    Collection<Balance> findByUserId(Integer userId);
 }
