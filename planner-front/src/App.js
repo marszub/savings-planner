fa4d31f582574fc7aacc4f9829ba364f40fda509
Ela Cymerys <ela.cymerys@gmail.com>
2022-04-09 17:20:26 +0200

    Update react-router-dom from v5 to v6

--- planner-front/src/App.js
+++ planner-front/src/App.js
@@ -1,15 +1,16 @@
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./Components/Auth/SignIn";
 import SignUp from "./Components/Auth/SignUp";
 
 function App() {
-  return (
-      <Router>
-        <Switch>
-          <Route path='/sign-in' component={ SignIn } />
-          <Route path='/sign-up' component={ SignUp } />
-        </Switch>
-      </Router>
+
+    return (
+        <Router>
+            <Routes>
+                <Route path='/sign-in' element={ <SignIn /> } />
+                <Route path='/sign-up' element={ <SignUp /> } />
+            </Routes>
+        </Router>
   );
 }
 
