21dd9ed832f7facc8bdd77875fa221c7f69cb2eb
Mikolaj <m_zasada@me.com>
2022-04-10 19:30:47 +0200

    Creating database structre of account balance and also cover them with tests

--- planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/dto/CreateBalanceResponse.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.agh.kuce.planner.balance.dto;
-
-import javax.validation.constraints.NotNull;
-
-public record CreateBalanceResponse (
-    @NotNull
-    Integer balance
-) {}
--- planner-api/src/test/java/pl/edu/agh/kuce/planner/balance/integration/BalanceTest.java
+++ planner-api/src/test/java/pl/edu/agh/kuce/planner/balance/integration/BalanceTest.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.kuce.planner.balance;
+package pl.edu.agh.kuce.planner.balance.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
