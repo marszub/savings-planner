c9c09a65803867e093a226d17ef2e8249765ad22
Ela Cymerys <ela.cymerys@gmail.com>
2022-04-10 19:19:00 +0200

    Add error page

--- planner-front/src/Components/Error.js
+++ planner-front/src/Components/Error.js
@@ -1,5 +1,36 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
 export default function Error() {
     return (
-        <h1>Error</h1>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="s">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h4">
+                        Aaaah! Something went wrong
+                    </Typography>
+                    <Typography component="h2" variant="h6">
+                        Brace yourself till we get the error fixed.
+                    </Typography>
+                    <Typography component="h2" variant="h6">
+                        You may also refresh the page or try again later
+                    </Typography>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
