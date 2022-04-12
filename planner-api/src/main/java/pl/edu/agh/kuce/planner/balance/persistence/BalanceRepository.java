9aa9c7e811c89e26abdd392592a5cae57cca7d98
Mikolaj <m_zasada@me.com>
2022-04-12 20:25:29 +0200

    Corrections in structure od db

--- planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/persistence/BalanceRepository.java
+++ planner-api/src/main/java/pl/edu/agh/kuce/planner/balance/persistence/BalanceRepository.java
@@ -1,12 +1,19 @@
 package pl.edu.agh.kuce.planner.balance.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pl.edu.agh.kuce.planner.auth.persistence.User;
 
-import java.util.Collection;
+import java.util.List;
 
+@Repository
 public interface BalanceRepository extends JpaRepository<Balance, Integer> {
+    List<Balance> findByUser(User user);
 
-    @Query("SELECT b FROM Balance b WHERE b.userId = ?1")
-    Collection<Balance> findByUserId(Integer userId);
+    @Modifying
+    @Query("UPDATE Balance b SET b.balance = :balance WHERE b.user = :user")
+    void updateBalance(@Param("user") User user, @Param("balance") Integer balance);
 }
