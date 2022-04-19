e3508b6da2477a28128721d4ede5a7d57bd5f012
paula192837465 <p.adamczyk095@gmail.com>
2022-04-19 14:04:57 +0200

    add finished goal component to HomePage

--- planner-front/src/Components/Goals.js
+++
@@ -1,9 +0,0 @@
-import Container from "@mui/material/Container";
-
-export default function Goals(){
-    return(
-        <Container>
-            <a>Cele</a>
-        </Container>
-    )
-}
--- planner-front/src/Components/Home.js
+++ planner-front/src/Components/Home.js
@@ -2,10 +2,10 @@ import Cashflow from "./Cashflow";
 import Timeline from "./Timeline";
 import Grid from "@mui/material/Grid";
 import Events from "./Events"
-import Goals from "./Goals"
 import "../styles/Home.css"
 import { useState, useEffect } from "react";
 import { eventService } from "../services/event-service";
+import GoalList from "./Goal/GoalList";
 
 var initialState = 5000
 
@@ -23,13 +23,17 @@ export default function Home() {
         if(!loading){
             return (
                 <div id="main-container">
-                <Grid container spacing={2}>
+                <Grid container spacing={1}>
                     <Grid item xs={9}>
                         <div className="container">
                             <div className="label-row title">
                                 <a>Cash Flow</a>
                             </div>
                             <Cashflow/>
+                            
+                        </div>
+                        <div className="container" style = {{marginTop : '15px'}}>
+                            <Timeline/>
                         </div>
                     </Grid>
                     <Grid item xs={3}>
@@ -37,12 +41,7 @@ export default function Home() {
                             <h2>Saldo: {initialState}</h2>
                         </div>
                         <div className="container goals">
-                            <Goals></Goals>
-                        </div>
-                    </Grid>
-                    <Grid item xs={9}>
-                        <div className="container">
-                            <Timeline/>
+                            <GoalList></GoalList>
                         </div>
                     </Grid>
                     <Grid item xs={12}>
