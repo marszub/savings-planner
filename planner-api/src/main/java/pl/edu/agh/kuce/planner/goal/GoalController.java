9f5a573821a878a9edb06c787539d318a771f1c6
MaksymilianK <kondziugeniusz@gmail.com>
2022-04-20 12:30:32 +0200

    Add Transaction and change OK to NO_CONTENT

--- planner-api/src/main/java/pl/edu/agh/kuce/planner/goal/GoalController.java
+++ planner-api/src/main/java/pl/edu/agh/kuce/planner/goal/GoalController.java
@@ -1,12 +1,7 @@
 package pl.edu.agh.kuce.planner.goal;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.agh.kuce.planner.auth.Current;
 import pl.edu.agh.kuce.planner.auth.persistence.User;
 import pl.edu.agh.kuce.planner.goal.dto.ListResponse;
@@ -25,6 +20,7 @@ public class GoalController {
     }
 
     @PostMapping("/goal")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void create(@Valid @RequestBody final GoalInputData data, @Current final User user) {
         goalService.create(data, user);
     }
@@ -35,6 +31,7 @@ public class GoalController {
     }
 
     @DeleteMapping("goal/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable("id") final Integer goalId,
                         @Current final User user) throws GoalNotFoundException {
         goalService.destroy(goalId, user);
