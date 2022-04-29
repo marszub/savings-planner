42dae43cad8e5d60033933f73cabed79dedd77c6
Marcin Szubert <marszub@gmail.com>
2022-04-29 14:43:05 +0200

    Add update and delete paths to Event API

--- planner-api/src/main/java/pl/edu/agh/kuce/planner/event/EventController.java
+++ planner-api/src/main/java/pl/edu/agh/kuce/planner/event/EventController.java
@@ -1,10 +1,13 @@
 package pl.edu.agh.kuce.planner.event;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.agh.kuce.planner.auth.Current;
@@ -13,6 +16,7 @@ import pl.edu.agh.kuce.planner.event.dto.ListResponse;
 import pl.edu.agh.kuce.planner.event.dto.OneTimeEventData;
 import pl.edu.agh.kuce.planner.event.dto.OneTimeEventDataInput;
 import pl.edu.agh.kuce.planner.event.service.EventService;
+import pl.edu.agh.kuce.planner.shared.ResourceNotFoundException;
 
 import javax.validation.Valid;
 
@@ -35,5 +39,19 @@ public class EventController {
     @GetMapping("")
     public ListResponse list(@Current final User user) {
         return eventService.list(user);
+    }
+
+    @PutMapping("/{id}")
+    public OneTimeEventData update(@PathVariable("id") final Integer eventId,
+                                   @Valid @RequestBody final OneTimeEventDataInput data,
+                                   @Current final User user) throws ResourceNotFoundException {
+        return eventService.update(data, eventId, user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") final Integer eventId,
+                       @Current final User user) throws ResourceNotFoundException {
+        eventService.delete(eventId, user);
     }
 }
