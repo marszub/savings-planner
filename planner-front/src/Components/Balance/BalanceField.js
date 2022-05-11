04f3248e8bf53933925788c026ca2ce24c4c8933
Ela Cymerys <ela.cymerys@gmail.com>
2022-05-11 11:19:01 +0200

    Refactor code

--- planner-front/src/Components/Balance/BalanceField.js
+++ planner-front/src/Components/Balance/BalanceField.js
@@ -7,41 +7,41 @@ import { moneyValidators } from "../../utils/money-validators";
 import { moneyFormatter } from "../../utils/money-formatter";
 
 export default function BalanceField() {
-    const [amountErrorMessage, setAmountErrorMessage] = useState("");
+    const [balanceErrorMessage, setBalanceErrorMessage] = useState("");
 
-    const defaultAmount = "0.00";
+    const defaultBalance = "0.00";
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
         const data = new FormData(event.currentTarget);
         const formModel = new BalanceForm(
-            data.get('amount')
+            data.get('balance')
         );
 
-        const amountError = moneyValidators.validateAmount(formModel.amount);
-        setAmountErrorMessage(amountError);
-        if (amountError) {
+        const balanceError = moneyValidators.validateAmount(formModel.balance);
+        setBalanceErrorMessage(balanceError);
+        if (balanceError) {
             return;
         }
 
-        const amount = moneyFormatter.mapStringToPenniesNumber(formModel.amount);
-        event.currentTarget.amount.value = moneyFormatter.mapPenniesNumberToString(amount);
+        const balance = moneyFormatter.mapStringToPenniesNumber(formModel.balance);
+        event.currentTarget.balance.value = moneyFormatter.mapPenniesNumberToString(balance);
     };
 
     return (
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 2 }}>
             <TextField
                 label="Account Balance"
-                id="amount"
-                name="amount"
-                defaultValue={defaultAmount}
+                id="balance"
+                name="balance"
+                defaultValue={defaultBalance}
                 sx={{ m: 1, width: '26ch' }}
                 InputProps={{
                     endAdornment: <InputAdornment position="end">PLN</InputAdornment>,
                 }}
-                error={!!amountErrorMessage}
-                helperText={amountErrorMessage}
+                error={!!balanceErrorMessage}
+                helperText={balanceErrorMessage}
             />
             <Button type="submit" variant="contained" size="large" sx={{ mt: 1, height: '6.7ch' }}>Update</Button>
         </Box>
