7ed2cbfc446e50a674bff804c415acc7bcc21c32
Ela Cymerys <ela.cymerys@gmail.com>
2022-05-31 19:52:36 +0200

    Fix event create

--- planner-front/src/requests/create-event-request.js
+++ planner-front/src/requests/create-event-request.js
@@ -2,6 +2,7 @@ export class CreateEventRequest {
     constructor(title, amount, timestamp) {
         this.title = title;
         this.amount = amount;
+        this.isCyclic = false;
         this.timestamp = timestamp;
     }
 }
