bf6757033e4b180c474137adaffa8aebc07cb950
Ela Cymerys <ela.cymerys@gmail.com>
2022-05-31 20:00:19 +0200

    Add isCyclic field to event update request

--- planner-front/src/requests/update-event-request.js
+++ planner-front/src/requests/update-event-request.js
@@ -2,6 +2,7 @@ export class UpdateEventRequest {
     constructor(title, amount, timestamp) {
         this.title = title;
         this.amount = amount;
+        this.isCyclic = false;
         this.timestamp = timestamp;
     }
 }
